a8754b27166c6bdc9055b3be83f1dbea8bf57480
Nimasha Heelibathdeniya <110526792+heelibathdeniyahanb@users.noreply.github.com>
2024-10-15 10:20:50 +0530

    Create Add new item
    create add new item form
    install react toastify
    install axios
    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Navbar from "./components/navbar";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import NewItem from "./components/Restaurant Manager/NewItem";
 
 function App() {
   return (
     <>
-      <Navbar />
+    <Navbar />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/add-new-item" element={<NewItem/>}></Route>
+    </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
